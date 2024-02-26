76e9582061883f937bdbdd094be02466a8a8b5bf
Junio Chaves <68963163+13junio@users.noreply.github.com>
2024-02-26 12:46:59 -0300

    atualização server

--- server.ts
+++ server.ts
@@ -1,9 +1,8 @@
+import { AppDataSource } from './data-source';
 import express from 'express';
 import "dotenv/config";
-import userRoutes from './src/routes/userRoutes';
-import amanhecerRoutes from './src/routes/amanhecerRoutes';
-import cors from 'cors'
-import { AppDataSource } from './data-source';
+
+const app = express();
 
 AppDataSource.initialize()
   .then(() =>{
@@ -14,21 +13,4 @@ AppDataSource.initialize()
       
     })
   })
-const app = express();
-const PORT = 3001;
-app.use(cors({origin: 'http://localhost:5173'}))
-
-app.use('/api/users', userRoutes);
-app.use('/api/amanhecer', amanhecerRoutes);
-
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json')
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-require('./endpoints')(app)
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+  .catch((err) => console.log(err))
