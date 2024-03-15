7eaab36e5f7ba8c444d156cd1ca2e0c9b4a2b8ca
Junio Chaves <68963163+13junio@users.noreply.github.com>
2024-03-15 10:02:48 -0300

    feat: implementação Swagger

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import express, { Application, Router } from 'express';
 import {userRoutes} from "./routes/userRoutes";
 import { contactRouter } from "./routes/contactRoutes";
 import swaggerRouter from "./swagger";
-// import swaggerRouter from "./swagger"
+
 
 export const app: Application = express();
 
@@ -17,24 +17,3 @@ app.use('/contact', contactRouter);
 
 app.use('/docs', swaggerRouter)
 
-// const express = require('express');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./');
-
-// const app = express();
-
-// // Adicione a rota para a documentação Swagger
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// // Defina suas outras rotas aqui...
-
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000');
-// });
-
-
-// app.use('/api-docs', swaggerRouter); // Rota para acessar a documentação da API
-
-// app.use('/api-documentation', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocument))
-
-// app.use(cors({origin: 'http://localhost:5173'})
